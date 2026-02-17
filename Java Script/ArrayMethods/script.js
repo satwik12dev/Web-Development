7b87f1eeec7fecaa5f70192a532ff4bd9b08922c
Satwik Saxena <satwiksaxena41@gmail.com>
2026-02-17 22:36:33 +0530

    Methods

---
+++ Java Script/ArrayMethods/script.js
@@ -0,0 +1,31 @@
+let a = [1, 2, 3, 4, 5];
+
+console.log(a.length);
+
+console.log(a.toString);
+
+console.log(a.at(3));
+
+console.log(a.concat([6, 7, 8]));
+console.log(a.join("-"));
+console.log(a.pop());
+console.log(a);
+console.log(a.push(6));
+console.log(a);
+console.log(a.shift());
+console.log(a);
+console.log(a.unshift(0));
+console.log(a);
+
+console.log(a.copyWithin(0, 3, 5));
+
+console.log(a.flat());
+
+console.log(a.slice(1, 4));
+
+console.log(a.splice(2, 1, 10, 11));
+
+console.log(a.reverse());
+
+console.log(a.toSpliced());
+
